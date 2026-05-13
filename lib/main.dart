f2f0f6e76a544b55d853c71bc0440ae640524754
iammesina012 <iammesina012@gmail.com>
2026-05-13 18:49:42 +0800

    Remove unused import for flutter services in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:familee_dental/features/auth/pages/login_page.dart';
